c79456aa1e30f53a6153c5aeb2979c8cf2b3f6bb
Kasun Hewage <kasun.ch@gmail.com>
2020-11-20 19:53:47 +0100

    Test

--- build.py
+++ build.py
@@ -1,12 +1,18 @@
 from cpt.packager import ConanMultiPackager
 from cpt.ci_manager import CIManager
 from cpt.printer import Printer
-import re
+import sys, re
+
+def hidesensitive(output):
+    output_str = str(output)
+    output_str = re.sub(r'(CONAN_LOGIN_USERNAME[_\w+]*)=\"(\w+)\"', r'\1="xxxxxxxx"', output_str)
+    output_str = re.sub(r'(CONAN_PASSWORD[_\w+]*)=\"(\w+)\"', r'\1="xxxxxxxx"', output_str)
+    sys.stdout.write(output_str)
 
 if __name__ == "__main__":
-    printer = Printer()
+    printer = Printer(hidesensitive)
     ci_man = CIManager(printer)
     version = re.sub(".*/", "", ci_man.get_branch())
-    builder = ConanMultiPackager(reference="qtserialport/%s" % version)
+    builder = ConanMultiPackager(reference=("qtserialport/%s" % version), out=hidesensitive)
     builder.add_common_builds(pure_c=False)
     builder.run()
