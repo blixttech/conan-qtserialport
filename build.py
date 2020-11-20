36fab1acb2851f5ef18312fd5ef971925576f810
Kasun Hewage <kasun.ch@gmail.com>
2020-11-20 22:51:51 +0100

    Catch all exceptions.

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from cpt.packager import ConanMultiPackager
 from cpt.ci_manager import CIManager
 from cpt.printer import Printer
 import sys, re
+import traceback
 
 def hidesensitive(output):
     output_str = str(output)
@@ -10,9 +11,13 @@ def hidesensitive(output):
     sys.stdout.write(output_str)
 
 if __name__ == "__main__":
-    printer = Printer(hidesensitive)
-    ci_man = CIManager(printer)
-    version = re.sub(".*/", "", ci_man.get_branch())
-    builder = ConanMultiPackager(reference=("qtserialport/%s" % version), out=hidesensitive)
-    builder.add_common_builds(pure_c=False)
-    builder.run()
+    try:
+        printer = Printer(hidesensitive)
+        ci_man = CIManager(printer)
+        version = re.sub(".*/", "", ci_man.get_branch())
+        builder = ConanMultiPackager(reference=("qtserialport/%s" % version), out=hidesensitive)
+        builder.add_common_builds(pure_c=False)
+        builder.run()
+    except Exception as e:
+        hidesensitive(traceback.format_exc())
+        hidesensitive(str(e))
