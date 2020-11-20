da69b1b3c4c4130d833a595f3376bfba8cb24820
Kasun Hewage <kasun.ch@gmail.com>
2020-11-20 18:51:49 +0100

    Version is set from the git branch taken from the CI manager

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
 from cpt.packager import ConanMultiPackager
+from cpt.ci_manager import CIManager
+from cpt.printer import Printer
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    printer = Printer()
+    ci_man = CIManager(printer)
+    builder = ConanMultiPackager(reference="qtserialport/%s" % ci_man.get_branch())
     builder.add_common_builds(pure_c=False)
     builder.run()
