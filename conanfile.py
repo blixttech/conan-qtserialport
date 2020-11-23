ab0825ac5510ad142efb08e4702b374303f1454c
Kasun Hewage <kasun.ch@gmail.com>
2020-11-23 14:28:48 +0100

    new changes

--- conanfile.py
+++ conanfile.py
@@ -15,3 +15,17 @@ class QtSerialPortConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = {"shared": True}
+
+    git_is_dirty = False
+    git_commit = "unknown"
+
+    def set_version(self):
+        git = tools.Git(folder=self.recipe_folder)
+        output = git.run("describe --all").splitlines()[0].strip()
+        self.version = re.sub("^.*/v?", "", output)
+        output = git.run("diff --stat").splitlines()
+        self.git_is_dirty = True if output else False
+        self.git_commit = git.run("rev-parse HEAD").splitlines()[0].strip()
+
+        self.output.info("Version: %s, Commit: %s, Is_dirty: %s" %
+                         (self.version, self.git_commit, self.git_is_dirty))
