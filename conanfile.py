c0da33ea5629fe455d48757ad7cfebc1ac5c718d
Kasun Hewage <kasun.ch@gmail.com>
2020-11-23 22:37:33 +0100

    Changed qtmodulepyreq channel

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class QtSerialPortConan(ConanFile):
     homepage = "https://code.qt.io/cgit/qt/qtserialport.git"
     license = "	LGPL-3.0-only"  # SPDX Identifiers https://spdx.org/licenses/
 
-    python_requires = "qtmodulepyreq/0.1.0"
+    python_requires = "qtmodulepyreq/0.1.0@blixt/stable"
     python_requires_extend = "qtmodulepyreq.QtModuleConanBase"
 
     settings = "os", "arch", "compiler", "build_type"
